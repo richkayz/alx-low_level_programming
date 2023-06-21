6c86d862e21ea70f9485fa8ead3152a150b2e7e1
Richard <richkayz@gmail.com>
2023-06-21 06:12:50 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,10 @@
 int main(void)
 {
 	char alphabet;
-
 	for (alphabet = 'a';alphabet <= 'z'; alphabet++)
-		putchar(alphabet);
+		_putchar(alphabet);
 
-	putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }
