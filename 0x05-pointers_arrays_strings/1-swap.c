af2066ad7fa74cb5d7e80db8af09810e0a843020
Richard <richkayz@gmail.com>
2023-06-26 11:12:25 -0700

    #include <stdio.h>
    /**
     *1-swap.c - swaps the values of two integers
     *
     *@a: initial integer to be swapped
     *
     *@b: Integer to be swapped
     *
     *Return: 0
    */
    void swap_int(int *a, int *b)
    {
    
        int temp;
        temp = *a;
        *a = *b;
        *b = temp;
    
    }

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /**
- *swap.c - swaps the values of two integers
+ *1-swap.c - swaps the values of two integers
  *
- *@a & @b: Integers to be swapped
+ *@a: initial integer to be swapped
+ *
+ *@b: Integer to be swapped
  *
  *Return: 0
 */
 void swap_int(int *a, int *b)
 {
 
-        int temp;
-        temp = *a;
-        *a = *b;
-        *b = temp;
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 
 }
