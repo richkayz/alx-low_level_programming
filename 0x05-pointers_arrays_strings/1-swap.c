1cc7a7e20af7091b503e9a94dd9402383fd38849
Richard <richkayz@gmail.com>
2023-06-26 11:30:11 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,11 @@
 /**
  *1-swap.c - swaps the values of two integers
  *
- *swap_int: Swaps the two integers function
- *
  *@a: initial integer to be swapped
  *
  *@b: Integer to be swapped
+ *
+ *swap_int: Swaps the two integers function
  *
  *Return: 0
 */
