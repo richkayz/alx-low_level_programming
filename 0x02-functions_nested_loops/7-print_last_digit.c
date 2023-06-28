09e33c4f9edce729f77c866ebee2da0f0815e398
root <root@Katxy.localdomain>
2023-06-28 21:47:59 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,10 @@
 #include "main.h"
-#include <conio.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  *  print_last_digit - last digit
  *
  * @n: The int to print
  * Return: Always 0.
  */
-
 int print_last_digit(int n)
 
 {
