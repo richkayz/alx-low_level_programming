61f8cf077771c117e17ec34cb0459cfc61bbfaa9
richkayz <richkayz@gmail.com>
2023-06-28 10:18:12 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- *print_alphabet - print alphabet lowercase
- *
- *Return: Always 0
- */
+*print_alphabet - print alphabet lowercase
+*
+*print_alphabet_x10 - Print alphabet 10 times
+*
+*Return: Always 0
+*/
 
 void print_alphabet_x10(void)
 {
