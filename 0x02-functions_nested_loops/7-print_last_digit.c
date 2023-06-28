5941224056cb9ec4d03b1e381abe6362e72ffe9e
root <root@Katxy.localdomain>
2023-06-28 21:17:39 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,8 +3,8 @@
 /**
  *  print_last_digit - last digit
  *
- * @n: int to be print
- * Return: 0.
+ * @n: The int to print
+ * Return: Always 0.
  */
 
 int print_last_digit(int n)
@@ -12,8 +12,16 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = n%10;
-	_putchar(last_digit);
-	return(last_digit);
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 
-}
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
