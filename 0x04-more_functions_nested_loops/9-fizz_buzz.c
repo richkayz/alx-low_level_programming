827a3e97426ba627e7f4242df7b9cd251aaa2163
root <root@Katxy.localdomain>
2023-06-30 22:49:14 +0300

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,38 @@
-#include "main.h"
 #include<stdio.h>
 /**
  * print 1-100 - Print 1-100
  * @n: number of times the printed
  * Description: Can use all functions
  */
- int main() {
+ int main(void) {
     int n = 1;
  
    //n is less than or equal to 100
-   while( n  <= 100) {
+   while( n  <= 100) 
+   {
    
-   return 0;
-
-   if (n % 3 == 0){
+            if (n % 3 == 0)
                     printf("Fizz");
-    }
-    elseif(n % 5 == 0){
-        printf("Buzz");
-    }
-    elseif(n % 3 == 0 && n % 5 == 0){
-        printf("FizzBuzz");
-   }
-   else{
-        //print the value of n
-      _putchar("%d ", ni);
- 
-      //Increment the value of n
-      i++;
-        }
-   }
+                
+                else if(n % 5 == 0)
+                    printf("Buzz");
+                
+                else if(n % 3 == 0 && n % 5 == 0);
+                    printf("FizzBuzz");
+            
+            else
+                    //print the value of n
+                printf("%d", n);
+
+                if (n != 100)
+                    printf(" ");
+            
+                //Increment the value of n
+                i++;
+                    
+            }
     //print new line on comletion of loop
-    _putchar('\n');
+    _putchar("\n");
     
    return 0;
 }
