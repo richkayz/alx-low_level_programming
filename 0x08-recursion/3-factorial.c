f80c8560c1fab0811bbe7b41e821f390aaf86a07
root <root@Katxy.localdomain>
2023-07-06 20:59:30 +0300

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _factorial - print factorial of int n.
+ * @n: the string to be printed
+ *
+ */
+int main(void)
+{
+	int n, i;
+	if (n < 0)
+	{
+		return(-1);
+	}
+	else
+	{
+		for (i = 1; i <= n; ++i)
+		{
+			 n *= i;
+			_putchar(n);
+		}
+		_putchar('\n');
+	return(0);}
+}
