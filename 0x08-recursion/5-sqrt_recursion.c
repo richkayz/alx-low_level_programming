e14120f917212c4039df78fcb445c83390415c5a
root <root@Katxy.localdomain>
2023-07-09 20:27:59 +0300

    natural square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,31 @@
 #include "main.h"
 
 /**
+ * test - Finds the square root of two numbers
+ *
+ * @a: The number
+ * @b: The number to test for the square root of @a
+ *
+ * Return: square root
+ */
+
+int test(int a, int b)
+{
+	if (b * b > a)
+		return (-1);
+
+	else if (b * b == a)
+		return (b);
+
+	else
+		return (test(a, b + 1));
+
+	return (1);
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  *
-* @a: The number
- * @b: The number to test for the square root of @a
- * 
  * @n: the number to return the square root of
  *
  * Return: the square root of @n
@@ -13,27 +33,7 @@
 
 int _sqrt_recursion(int n)
 {
-
-    int a, b;
-    if (b * b > a)
-    {
-        return (-1);
-
-    }
-	else if (b * b == a)
-    {
-            return (b);
-    }
-	else
-    {
-        	return (a, b + 1);
-
-	return (1);
-    }
-	
-	if (n == 0){
-        return (0);
-    }
-		
-	return (n, 1);
+	if (n == 0)
+		return (0);
+	return (test(n, 1));
 }
