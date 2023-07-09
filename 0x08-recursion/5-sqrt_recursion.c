9dbddfcf9fe5ede523bb936ff77c0f33254a25f5
root <root@Katxy.localdomain>
2023-07-09 20:09:07 +0300

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+* @a: The number
+ * @b: The number to test for the square root of @a
+ * 
+ * @n: the number to return the square root of
+ *
+ * Return: the square root of @n
+ */
+
+int _sqrt_recursion(int n)
+{
+
+    int a, b;
+    if (b * b > a)
+    {
+        return (-1);
+
+    }
+	else if (b * b == a)
+    {
+            return (b);
+    }
+	else
+    {
+        	return (helper(a, b + 1));
+
+	return (1);
+    }
+	
+	if (n == 0){
+        return (0);
+    }
+		
+	return (n, 1);
+}
